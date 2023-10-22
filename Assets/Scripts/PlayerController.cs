4c1ae19cc0ce9186b12be0c8f8939dd1b79fa973
Sakti Harahap <saktiharahap2606@gmail.com>
2023-10-22 11:01:32 +0700

    Add playerController script add assetPlayer
    add tagManager Ground

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 8f;
+    [SerializeField] float jumpingPower = 16f;
+    [SerializeField] bool isFacingRight = true;
+
+    private bool doubleJump;
+
+     private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Movement();
+        Jump();
+    }
+
+    void Movement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        if(horizontal < 0 && isFacingRight)
+        {
+            Quaternion rotateLeft =  Quaternion.Euler(0, 180, 0);
+            transform.rotation = rotateLeft;
+            isFacingRight = false;
+        }
+        else if( horizontal > 0 && !isFacingRight) 
+        {
+            Quaternion rotateRight = Quaternion.Euler(Vector3.zero);
+            transform.rotation = rotateRight;
+            isFacingRight = true;
+        }
+    }
+
+    void Jump()
+    {
+        if (IsGrounded() && !Input.GetButton("Jump"))
+        {
+            doubleJump = false;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (IsGrounded() || doubleJump)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+
+                doubleJump = !doubleJump;
+
+                Debug.Log(doubleJump);
+            }
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+}
